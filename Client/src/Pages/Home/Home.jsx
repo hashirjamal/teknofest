8f0fd6eacc5d38e85e8e24a4bf2abc981ae3af50
Hashir <hashirjamal18@gmail.com>
2024-10-13 11:36:02 +0500

    Starting home page

---
+++ Client/src/Pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import gif from "../../assets/Images/website.gif"
+export default function Home() {
+  return (
+    <div>
+        <section>
+            <div>
+            <img src={gif}/>
+<div>
+    <h3>Upcoming Event </h3>
+    <h1>TeknoFest Pakistan</h1>
+    <p>12th and 13th October</p>
+    <p>Expo Center Karachi, Hall 3 & 4</p>
+    <p>Empowering innovation, igniting future leaders. Teknofest Pakistan – where technology meets limitless possibilities</p>
+</div>
+            </div>
+        </section>
+
+    </div>
+  )
+}
