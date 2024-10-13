17874df7ff7cc59f9b319181bd0f46877a4eb3a7
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-10-13 11:29:32 +0500

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/competitions",
+    element: <CompetitionsPage />
+  },
+  {
+    path: "/competitions/:name",
+    element: <CompetitionPage />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
