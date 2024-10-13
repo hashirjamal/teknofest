65da1ecf702586b79978b747ce5966c14aebe1ea
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-10-13 13:00:21 +0500

    Update CarouselBoxes.jsx

--- Client/src/components/CarouselBoxes.jsx
+++ Client/src/components/CarouselBoxes.jsx
@@ -1,31 +1,32 @@
 import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@mui/material'
 
-function Example(props)
+function Example({competitions})
 {
-    var items = [
-      [  {
-            name: "Graphics and Design",
-            description: "Dive into the world of graphics and animation."
-        },
-        {
-            name: "Random Name #2",
-            description: "Hello World!"
-        }],
-      [  {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
-        },
-        {
-            name: "Random Namedasfdsfsdfsd#2",
-            description: "Hello World!"
-        }]
-    ]
+    console.log(competitions);
+    // var items = [
+    //   [  {
+    //         name: "Graphics and Design",
+    //         description: "Dive into the world of graphics and animation."
+    //     },
+    //     {
+    //         name: "Random Name #2",
+    //         description: "Hello World!"
+    //     }],
+    //   [  {
+    //         name: "Random Name #1",
+    //         description: "Probably the most random thing you have ever seen!"
+    //     },
+    //     {
+    //         name: "Random Namedasfdsfsdfsd#2",
+    //         description: "Hello World!"
+    //     }]
+    // ]
 
     return (
         <Carousel>
             {
-                items.map( (item, i) => <div className="flex flex-row items-center justify-center align-middle w-screen gap-2 basis-1/2">
+                competitions.map( (item, i) => <div className="flex flex-row items-center justify-center align-middle w-screen gap-2 basis-1/2">
                     <Item key={i} item={item[0]} />
                     <Item key={i} item={item[1]} />
                     </div> )
